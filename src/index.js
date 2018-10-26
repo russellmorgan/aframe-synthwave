2959826b6df28e6ea4efdc2a7043ca513ab7396b
Russell Morgan <russell_morgan@comcast.com>
2018-10-26 16:34:53 -0400

    Added music, wireframe shader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('ready!!!!');
+import aframe from 'aframe';
+
+// If I want to make anything wireframe...
+AFRAME.registerComponent('wireframe', {
+  dependencies: ['material'],
+  init: function () {
+    this.el.components.material.material.wireframe = true;
+  }
+});
